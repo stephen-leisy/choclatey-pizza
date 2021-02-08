2acc5a60697f9658cca830e43e58c0da8b93da68
stephen-leisy <stleisy@gmail.com>
2021-02-08 15:28:48 -0800

    Added a header and a picture.

---
+++ src/PageHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class PageHeader extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>Delicious</h1>
+                <button>links</button>
+                <button>links</button>
+                <button>links</button>
+            </header>
+
+        );
+    }
+}
