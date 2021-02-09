e5f488014509e2f14b1dae537d7f76378191de68
stephen-leisy <stleisy@gmail.com>
2021-02-08 17:22:26 -0800

    Added p tags and created data.js.

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const Ingredients = [
+    {
+        name: 'Milk',
+        amount: '1-1/2 Cups',
+    }, {
+        name: 'Mascarpone',
+        amount: '1/2 Cup',
+    }, {
+        name: 'Pink Salt',
+        amount: '1/2 tsp',
+    }, {
+        name: 'Black Mission Figs',
+        amount: '1 lb',
+    }, {
+        name: 'brown Sugar',
+        amount: '1/2 Cup',
+    }, {
+        name: 'water',
+        amount: '2-4 tbsp',
+    }, {
+        name: 'Heavy Cream',
+        amount: '1-1/2 Cups',
+    }, {
+        name: 'granulated sugar',
+        amount: '1/3 Cup',
+    }, {
+        name: 'egg yolks',
+        amount: '2',
+    }, {
+        name: 'lemon (juiced)',
+        amount: '1',
+    }, {
+        name: 'butter',
+        amount: '2 tbsp',
+    }, {
+        name: 'honey roasted pecans, roughly chopped',
+        amount: '1 cup',
+    }
+]
